5f1718eb2a223cc0d88714e126bee81a8b038ee0
Guerizim <gueri_leon@outlook.com>
2024-03-06 13:23:50 -0800

    Added links

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,1 +1,21 @@
 const Skill = require('../models/skill');
+
+module.exports = {
+  index,
+  show
+}
+
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll(),
+    title: 'All Skills'
+  });
+};
+
+function show(req, res) {
+  console.log(req.params)
+  res.render('skills/show', {
+    skill: Skill.getOne(req.params.skillsId),
+    title: 'Skill Details'
+  });
+}
--- routes/skills.js
+++ routes/skills.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 // GET all skills
+router.get('/', skillsCtrl.index);
+
+// GET one todo -->  /skills/:id
+router.get('/:skillsId', skillsCtrl.show)
 
 module.exports = router;
